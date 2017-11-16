16cb70b781358bd425ff1dbdb5cd365a42327901
jithinbp <jithinbp@gmail.com>
2017-11-16 11:12:24 +0530

    stirling code calculates up to N=2000

--- Code/Physics/statistical/stirling.py
+++ Code/Physics/statistical/stirling.py
@@ -1,12 +1,13 @@
-# Validity of stirling's approximation
+# Validity of stirling's approximation up to N=2000
 import math
 
 def stirling(N):  #approximation function. log(n!)  = nlog(n)-n
     return N*math.log(N) - N
 errors=[]
 factN=1
+MAXNUM = 2001
 print('N\tActual\tApprox\t%Error')
-for N in range(1,110): # loop from N=1 to N=99
+for N in range(1,MAXNUM): # loop from N=1 to N=99
 	factN *= N  # factorial is automatically calculated and revised per iteration.	
 	actual = math.log(factN)
 	approx = stirling(N)
@@ -15,11 +16,13 @@ for N in range(1,110): # loop from N=1 to N=99
 		err = 100*(actual-approx)/actual
 		errors.append(err)
 		#Print values to the screen every 5 iterations
-		if N%5==0:print ('%d\t%.2f\t%.2f\t%.2f'%(N,actual, approx, err))
+		if N<5: print ('%d\t%.2f\t%.2f\t%.4f'%(N,actual, approx, err))
+		elif N<100 and N%20==0: print ('%d\t%.2f\t%.2f\t%.4f'%(N,actual, approx, err))
+		elif N%100==0:print ('%d\t%.2f\t%.2f\t%.4f'%(N,actual, approx, err))
 
 #Plot N vs %error 
 from pylab import *
-# x = [3 ... 119] , y = errors
-plot(range(3,110),errors)
+# x = [3 ... MAXNUM-1] , y = errors
+plot(range(3,MAXNUM),errors)
 xlabel('N');ylabel('percentage error')
 show()
